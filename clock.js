bad197dd0f582bfe7370711e94d768e6ee6b0974
Aman_Prajapat <amanprajapat98801@gmail.com>
2021-10-09 19:33:33 +0530

    commit all files

---
+++ clock.js
@@ -0,0 +1,14 @@
+setInterval(() => {
+   let  date = new Date();
+    hours = date.getHours();
+    minutes = date.getMinutes();
+    seconds = date.getSeconds();
+
+    hour_rotation = 30*hours+minutes/2;
+    minute_rotation = 6*minutes;
+    second_rotation = 6*seconds;
+
+    hour.style.transform = `rotate(${hour_rotation}deg)`;
+    minute.style.transform = `rotate(${minute_rotation}deg)`;
+    second.style.transform = `rotate(${second_rotation}deg)`;
+}, 1000);
